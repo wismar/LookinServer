7850c786e021328a53a5fcb8d6c90bc836009ce9
wismar <wismar@users.noreply.github.com>
2023-03-09 21:29:58 -0500

    Update Package.swift
    remove debug req

--- Package.swift
+++ Package.swift
@@ -30,8 +30,8 @@ let package = Package(
                 .headerSearchPath("**")
             ],
             cxxSettings: [
-                .define("SHOULD_COMPILE_LOOKIN_SERVER", to: "1", .when(configuration: .debug)),
-                .define("SPM_LOOKIN_SERVER_ENABLED", to: "1", .when(configuration: .debug))
+                .define("SHOULD_COMPILE_LOOKIN_SERVER", to: "1"),
+                .define("SPM_LOOKIN_SERVER_ENABLED", to: "1")
             ]
         ),
         .target(
@@ -39,12 +39,12 @@ let package = Package(
             dependencies: [.target(name: "LookinServerBase")],
             path: "Src/Swift",
             cxxSettings: [
-                .define("SHOULD_COMPILE_LOOKIN_SERVER", to: "1", .when(configuration: .debug)),
-                .define("SPM_LOOKIN_SERVER_ENABLED", to: "1", .when(configuration: .debug))
+                .define("SHOULD_COMPILE_LOOKIN_SERVER", to: "1"),
+                .define("SPM_LOOKIN_SERVER_ENABLED", to: "1")
             ],
             swiftSettings: [
-                .define("SHOULD_COMPILE_LOOKIN_SERVER", .when(configuration: .debug)),
-                .define("SPM_LOOKIN_SERVER_ENABLED", .when(configuration: .debug))
+                .define("SHOULD_COMPILE_LOOKIN_SERVER"),
+                .define("SPM_LOOKIN_SERVER_ENABLED")
             ]
         ),
         .target(
@@ -53,7 +53,7 @@ let package = Package(
             path: "Src/Base",
             publicHeadersPath: "",
             cxxSettings: [
-                .define("SHOULD_COMPILE_LOOKIN_SERVER", to: "1", .when(configuration: .debug))
+                .define("SHOULD_COMPILE_LOOKIN_SERVER", to: "1")
             ]
         )
     ]
